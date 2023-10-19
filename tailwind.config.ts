88f1ca9d04fb1769144f9c4af33524680e929533
Quyen Hoang <qduhoang@gmail.com>
2023-10-18 19:28:35 -0700

    added google fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        navFont: ['Inter', 'sans-serif'],
+        bodyFont: ['Open Sans', 'sans-serif'],
+      },
+      boxShadow: {
+        main: '0 8px 26px -4px hsla(0,0%,8%,.15),0 8px 9px -5px hsla(0,0%,8%,.06)',
+        'white-sm':
+          '0 2px 3px -1px rgba(255, 255, 255, 0.1), 0 1px 2px -1px rgba(255, 255, 255, 0.1)',
+        'white-md':
+          '0 4px 6px -1px rgba(255, 255, 255, 0.1), 0 2px 4px -2px rgba(255, 255, 255, 0.1)',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
